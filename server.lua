f5e504953df244a31a0591b5a8d4d822d179b27a
SungJinKang <33873804+SungJJinKang@users.noreply.github.com>
2019-10-16 23:58:46 +0900

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
--- how to use this
+-- how to use this 
 
--- Create Pickup
-TriggerClientEvent('ews:createPickup', -1, { id = 'PICKUP_WEAPON_MUSKET', blipId = 158 }, {x = 1, y = 1, z = 1}, 1 ) 
+-- Create Pickup (EXAMPLE)
+TriggerClientEvent('ews:createPickup', -1, { id = 'PICKUP_WEAPON_MUSKET', blipId = 158 }, {x = 1, y = 1, z = 1}, 1 ) -- { id : pickupid, blipid : blipid }, { coords }, ammount
 
--- Destory Pickup
+-- Destory Pickup (EXAMPLE)
 TriggerClientEvent('ews:removeAllPickup', -1)
